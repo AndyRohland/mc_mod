fda9e3be06074cad8c7b7a23f23580b94e4fa63a
AndyRohland <147657971+AndyRohland@users.noreply.github.com>
2023-10-11 20:12:41 +0000

    example adding program

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "iostream"
+
+int main()
+{
+    int a;
+    int b;
+
+    std::cin >> a;
+    std::cin >> b;
+    
+    int c = a + b;
+
+    std::cout << c << std::endl;
+
+    return 0;
+}
+
+
